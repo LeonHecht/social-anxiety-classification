de83ff79715fb6be9d5aa434effa12dca6fc77f5
LeonHecht <50410878+LeonHecht@users.noreply.github.com>
2024-04-17 20:32:29 -0600

    added Custom Weighted Model to combat class imbalance, added differencial loss callback to stop when overfitting, added 2-step classification

--- utils/paraphrase.py
+++ utils/paraphrase.py
@@ -4,12 +4,15 @@ from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 def paraphrase(texts, keywords):
 
     # Define a function for paraphrasing
-    def paraphrase_text(text, max_length=62):
+    def paraphrase_text(text, keyword, max_length=60):
         # Encode the input text
-        input_ids = tokenizer.encode(text, return_tensors="pt", truncation=True, max_length=max_length)
+        print("Type of text:", type(text))
+        print("text:", text)
+
+        input_ids = tokenizer.encode(f"paraphrase (your response should include the outdoor activity keyword '{keyword}'): " + text, return_tensors="pt", truncation=True, max_length=max_length)
 
         # Generate paraphrases
-        paraphrases = model.generate(input_ids, num_return_sequences=8, max_length=max_length)
+        paraphrases = model.generate(input_ids, do_sample=True, temperature=1.5, num_beams=100, num_return_sequences=5, max_length=max_length)
 
         # Decode the generated paraphrases
         paraphrased_texts = [tokenizer.decode(p, skip_special_tokens=True) for p in paraphrases]
@@ -24,7 +27,7 @@ def paraphrase(texts, keywords):
     paraphrased_texts_final = []
     for i in range(len(texts)):
         print(f"Paraphrasing text {i}/{len(texts)}")
-        paraphrased_texts = paraphrase_text(texts[i])
+        paraphrased_texts = paraphrase_text(texts[i], keywords[i])
         print(f"paraphrased_texts {i}:", paraphrased_texts)
         paraphrased_texts_final.append(paraphrased_texts)
     print("paraphrased_texts_final:\n", paraphrased_texts_final)
